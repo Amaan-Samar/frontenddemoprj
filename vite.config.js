9294c57742955f7d72dc07e68f7b1f9b3a23da55
amman.iqbal_geospatialdatatechniciandeev <Amman.iqbal@techmavengeospatial.com>
2025-11-14 17:56:29 +0500

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/frontenddemoprj/'
+})
+git config user.name "Amaan-Samar"
+git config user.email "asamariqbal@gmail.com"
